f207ecc519f3a9a4ff3ddb6c091e8e0e3ecd13e7
Fluffyfox <mlbbcommunity396@gmail.com>
2025-10-28 09:34:28 +0200

    Change Mega authentication credentials
    Updated Mega authentication credentials with new email and password.

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'malvinb017@gmail.com', // Replace with your Mega email
-    password: 'malvin266', // Replace with your Mega password
+    email: 'horizon.analytics.co@gmail.com', // Replace with your Mega email
+    password: 'GITHUB2020bl', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
